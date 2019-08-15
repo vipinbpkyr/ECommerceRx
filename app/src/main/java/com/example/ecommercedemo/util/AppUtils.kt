8ab2f506d64457aa8dcec46792629c0bb45a2ff1
Vipin B P <vipinbpkyr@gmail.com>
2019-08-15 18:38:08 +0530

    commit

--- app/src/main/java/com/example/ecommercedemo/util/AppUtils.kt
+++ app/src/main/java/com/example/ecommercedemo/util/AppUtils.kt
@@ -5,6 +5,10 @@ import android.graphics.BitmapFactory
 import android.graphics.Bitmap
 import java.io.IOException
 import java.io.InputStream
+import android.media.MediaMetadataRetriever
+import android.os.Environment
+import android.os.Environment.getExternalStorageDirectory
+import java.io.File
 
 
 fun getBitmapFromAsset(context: Context, filePath: String): Bitmap? {
@@ -21,4 +25,18 @@ fun getBitmapFromAsset(context: Context, filePath: String): Bitmap? {
     }
 
     return bitmap
+}
+
+fun getBitmapFromVide(): Bitmap{
+
+    val sdcard = Environment.getExternalStorageDirectory()
+    val file = File(sdcard, "myvid.mp4")
+    val retriever = MediaMetadataRetriever()
+    retriever.setDataSource(file.absolutePath);
+
+    val bm = retriever.getFrameAtTime(1000*1000,MediaMetadataRetriever.OPTION_CLOSEST)
+    retriever.release()
+    return bm
+
+
 }
